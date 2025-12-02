5cfc2b84be1a9536906db52f4fdfd536a47a08ec
Sander Claes <sanderclaes.dev@gmail.com>
2025-12-02 11:07:34 +0100

    Fix: imports

--- app/src/main/java/edu/ap/mobile_development_project/screens/CityOverviewScreen.kt
+++ app/src/main/java/edu/ap/mobile_development_project/screens/CityOverviewScreen.kt
@@ -20,6 +20,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
